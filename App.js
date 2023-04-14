d474f22a0bb0b8aca9b3e6e5be14d9f39994fe6a
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-14 11:03:12 -0500

    Implementando persistencia de datos con Redux

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { Provider } from "react-redux";
 import { MainNavigation } from "./src/nav/MainNavigation";
-import store from "./src/store/index"
+import { store, storePersisted } from "./src/store/index"
+import { PersistGate } from 'redux-persist/integration/react'
 
 export default function App() {
   return (
     <>
       <Provider store={store}>
-        <MainNavigation />
+        <PersistGate loading={null} persistor={storePersisted} >
+          <MainNavigation />
+        </PersistGate>
       </Provider>
     </>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,19 @@
 import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const configPersist = {
+  key: "root",
+  storage: AsyncStorage
+}
+
 import { reducer } from './reducers/card.reducer'
 
 const RootReducer = reducer
 
-export default createStore(RootReducer);
+const persistedReducer = persistReducer(configPersist, RootReducer)
+
+
+export const store = createStore(persistedReducer)
+
+export const storePersisted = persistStore(store)
